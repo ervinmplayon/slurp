232fb521a2c3c8d9fbd4acffc04357ffa853ae92
ervz <ervin.mitra@playonsports.com>
2025-05-24 00:49:06 -0400

    commit and stash

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ervinmplayon/slurp
+
+go 1.24.2
+
+require (
+	github.com/ervinmplayon/intercour-face-loggizle v0.0.0-20250523024227-46e8a85507f4 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/ervinmplayon/intercour-face-loggizle/logger"
 )
 
 // * returns a function that accepts another function as an argument
-func rateLimiter(interval time.Duration) func(func()) {
+func rateLimiter(interval time.Duration) func(func(l logger.LogrusLogger)) {
 	var lastTime time.Time
 
 	// * the function closure
-	return func(fn func()) {
+	return func(fn func(l logger.LogrusLogger)) {
 		now := time.Now()
 		if now.Sub(lastTime) >= interval {
 			lastTime = now
 			// * call the user-provided function, execute the logic that was passed in.
-			fn()
+			fn(l)
 		} else {
 			fmt.Println("Rate limit exceeded. Skiiping call.")
 		}
@@ -24,6 +26,7 @@ func rateLimiter(interval time.Duration) func(func()) {
 
 func main() {
 	limit := rateLimiter(2 * time.Second)
+	logger := logger.NewLogrusLogger()
 
 	for i := 0; i < 5; i++ {
 		/*
@@ -31,8 +34,8 @@ func main() {
 		 * Inside the limiter is what runs that Println.
 		 * It's deferring the execution of a block of logic, based on timing
 		 */
-		limit(func() {
-			fmt.Println("Function called at", time.Now().Format("15:04:05"))
+		limit(func(LogrusLogger) {
+			logger.Info("Function called at" + time.Now().Format("15:04:05"))
 		})
 		time.Sleep(1 * time.Second)
 	}
