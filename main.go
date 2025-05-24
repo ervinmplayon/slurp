ccdea3ca9b89b9d93138653c335d6989a4f5260b
ervz <ervin.mitra@playonsports.com>
2025-05-23 23:32:29 -0400

    purple daze

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"time"
 )
 
-// return a function that only allows execution once every `interval`
+// * returns a function that accepts another function as an argument
 func rateLimiter(interval time.Duration) func(func()) {
 	var lastTime time.Time
 
+	// * the function closure
 	return func(fn func()) {
 		now := time.Now()
 		if now.Sub(lastTime) >= interval {
 			lastTime = now
+			// * call the user-provided function, execute the logic that was passed in.
 			fn()
 		} else {
 			fmt.Println("Rate limit exceeded. Skiiping call.")
@@ -24,6 +26,11 @@ func main() {
 	limit := rateLimiter(2 * time.Second)
 
 	for i := 0; i < 5; i++ {
+		/*
+		 * func() {...}, is the argument passed into the rate limiter.
+		 * Inside the limiter is what runs that Println.
+		 * It's deferring the execution of a block of logic, based on timing
+		 */
 		limit(func() {
 			fmt.Println("Function called at", time.Now().Format("15:04:05"))
 		})
